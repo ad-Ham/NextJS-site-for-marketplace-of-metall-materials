ce9ccf2ac6cf00eabde769bb442808ccbe02f918
ad-Ham <aleks.khomenkov.03@mail.ru>
2022-03-22 18:50:32 +0300

    Finished profile page
    Finished profile page

--- pages/profile.js
+++ pages/profile.js
@@ -3,6 +3,8 @@ import {MainLayout} from '../components/MainLayout'
 import {Portions} from '../components/Portions'
 import {PersonalData} from '../components/PersonalData'
 import Adbannertop from '../public/adbannertop.svg'
+import Mypromosicon from '../public/mypromosicon.svg'
+import Submitpromoicon from '../public/submitpromoicon.svg'
 import Link from 'next/link'
 
 export default function Index() {
@@ -22,10 +24,44 @@ export default function Index() {
 					<Portions/>
 				</div>
 				<div className="rightside">
-
+					<div className="buttonsrow">
+						<Link href="/mypromos"><a className="profilebutton">
+							<Mypromosicon/>
+							<p>Мои объявления</p>
+						</a></Link>
+						<Link href="/submitpromo"><a className="profilebutton">
+							<Submitpromoicon/>
+							<p>Подать объявление</p>
+						</a></Link>
+					</div>
 				</div>
 			</div>
 			<style jsx>{`
+				.buttonsrow {
+					margin-top: 24px;
+					display: flex;
+					align-items: center;
+					justify-content: space-around;
+					border-bottom: 1px solid #848484;
+					padding-bottom: 18px;
+				}
+
+				.profilebutton {
+					width: 250px;
+					height: 250px;
+					background: #C4C4C4;
+					border-radius: 10px;
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					justify-content: space-between;
+					padding: 29px 0 18px 0;
+					font-weight: 500;
+					font-size: 24px;
+					line-height: 99.69%;
+					color: #000;
+				}
+
 				.adbannertop {
 					margin-top: 13px;
 				}
@@ -46,6 +82,7 @@ export default function Index() {
 				}
 
 				.rightside {
+					padding-left: 25px;
 					display: flex;
 					flex-direction: column;
 					width: 100%;
