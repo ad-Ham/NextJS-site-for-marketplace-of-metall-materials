5ee19def47bc277226c3e10b2c91b2d38541273e
Artur <study.artur@mail.ru>
2022-05-01 01:21:48 +0500

    Добавление GET запроса на Новости и Объявления

--- components/news/NewsPageMainNews.js
+++ components/news/NewsPageMainNews.js
@@ -1,18 +1,46 @@
 import Link from 'next/link'
+import { useState, useEffect } from 'react';
 import styles from './NewsPageMainNews.module.scss'
 
 export function NewsPageMainNews() {
-	return(<>
+
+	const [news, setNews] = useState([])
+
+	useEffect(() => {
+		fetch('http://localhost:3000/newsquery', {
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json'
+			}
+		})
+			.then(response => response.json())
+			.then(result => {
+				console.log(result);
+				setNews(result.data.news)
+			})
+			.catch(err => {
+				if (err) {
+					console.log(err);
+				}
+			})
+	}, [])
+
+	return (<>
 		<div className={styles.maindiv}>
 			<div className={styles.newsmainheader}>
 				<h1 className={styles.newsmainheadertext}>Новости</h1>
 			</div>
 			<Link href="/newspage"><a className={styles.a}><div className={styles.photo}></div></a></Link>
 			<div className={styles.importantnewsheader}>
-				<Link href="/newspage"><a><h2 className={styles.importantnewsheadertext}>Новость дня!</h2></a></Link>
+				<Link href="/newspage"><a>
+					{news.map(title => <h2>{title}</h2>)}
+					<h2 className={styles.importantnewsheadertext}>Новость дня!</h2></a></Link>
+				{news.map(date => <p>{date}</p>)}
 				<p className={styles.importantnewsheaderdate}>22.02 18:00</p>
 			</div>
+			{news.map(text => <p>{text}</p>)}
 			<p className={styles.importantnewstext}>В декабре 2021 года Россия импортировала 4 тыс. тонн медного лома и отходов, что на 22% больше, чем месяцем ранее, но на 38% меньше по сравнению с декабрем 2020 года. Средняя импортная цена за декабрь увеличилась на 1 $/т до 8,6 $/кг.</p>
+			{news.map(tags => <p>{tags}</p>)}
 			<p className={styles.importantnewsbottomtext}>Теги: импорт, лом, отходы, металлургия, цена, увеличилась</p>
 		</div>
 		<style jsx>{`
--- components/promopage/PromoBlock.js
+++ components/promopage/PromoBlock.js
@@ -1,27 +1,60 @@
 import styles from './PromoBlock.module.scss'
+import { useState, useEffect } from 'react';
 
 export function PromoBlock() {
-	return(<>
+
+	const [promos, setPromos] = useState([])
+
+	useEffect(() => {
+		fetch('http://localhost:3000/promosquery', {
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json'
+			}
+		})
+			.then(response => response.json())
+			.then(result => {
+				console.log(result);
+				setPromos(result.data.promos)
+			})
+			.catch(err => {
+				if (err) {
+					console.log(err);
+				}
+			})
+	}, [])
+
+	return (<>
 		<div className={styles.maindiv}>
 			<div className={styles.headerblock}>
+				{promos.map(title => <h1>{title}</h1>)}
 				<h1 className={styles.promoheader}>Металлические ограждения</h1>
 				<div className={styles.photo}></div>
 			</div>
 			<div className={styles.desc}>
 				<div className={styles.descrow}>
+					<p className={styles.descheader}>Страна:</p>
+					{promos.map(country => <p>{country}</p>)}
+					<p className={styles.desctext}>Россия</p>
+				</div>
+				<div className={styles.descrow}>
 					<p className={styles.descheader}>Регион:</p>
+					{promos.map(region => <p>{region}</p>)}
 					<p className={styles.desctext}>Владимир и Владимирская область</p>
 				</div>
 				<div className={styles.descrow}>
 					<p className={styles.descheader}>E-mail:</p>
+					{promos.map(email => <p>{email}</p>)}
 					<p className={styles.desctext}>example@mail.ru</p>
 				</div>
 				<div className={styles.descrow}>
 					<p className={styles.descheader}>Телефон:</p>
+					{promos.map(phoneNumber => <p>{phoneNumber}</p>)}
 					<p className={styles.desctext}>8-800-555-35-35</p>
 				</div>
 				<div className={styles.descrow}>
 					<p className={styles.descheader}>Организация:</p>
+					{promos.map(organizationName => <p>{organizationName}</p>)}
 					<p className={styles.desctext}>ООО Рога и копыта</p>
 				</div>
 				<div className={styles.descrow}>
@@ -30,7 +63,8 @@ export function PromoBlock() {
 				</div>
 				<div className={styles.descrow}>
 					<p className={styles.descheader}>Описание:</p>
-					<p className={styles.desctext}>Металлические ограждения 3D.<br/><br/>Для: автомобильных дорог, аэродромов, габионов, стадионов, промышленных объектов, гражданских объектов, коттеджей и дач.  </p>
+					{promos.map(desription => <p>{desription}</p>)}
+					<p className={styles.desctext}>Металлические ограждения 3D.<br /><br />Для: автомобильных дорог, аэродромов, габионов, стадионов, промышленных объектов, гражданских объектов, коттеджей и дач.  </p>
 				</div>
 			</div>
 		</div>
