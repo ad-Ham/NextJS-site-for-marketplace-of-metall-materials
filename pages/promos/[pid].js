b94d54b6a71e70719878790f68ea4a213e66d2b3
Restily <russianlolik@gmail.com>
2022-05-21 05:08:31 +0500

    add promo and news

--- pages/promos/[pid].js
+++ pages/promos/[pid].js
@@ -2,44 +2,19 @@ import Head from 'next/head'
 import { MainLayout } from '../../components/Layout/MainLayout'
 import { MainPromos } from '../../components/mainpage/MainPromos'
 import { MainPageNews } from '../../components/mainpage/MainPageNews'
-import { PromoBlock } from '../../components/promopage/PromoBlock'
+import { PromoBlock } from '../../components/promos/singlePromos/PromoBlock'
 import { MorePromosCard } from '../../components/promopage/MorePromosCard'
 import { Adbannertop } from '../../components/Adbannertop'
 import Adbannerside from '../../public/adbannerside.svg'
 import Link from 'next/link'
 import styles from '../../styles/promopage.module.scss'
 
-export default function Index() {
-	return (
-		<MainLayout>
-			<Head>
-				<title>Next Title</title>
-				<meta name="keywords" content="next, javascript" />
-				<meta name="description" content="this is" />
-				<meta charSet="utf-8" />
-			</Head>
-			{/* <Adbannertop/> */}
-			<div className={styles.content}>
-
-				<div className={styles.leftside}>
-					{/* <div className={styles.adbannerside}><Link href="https://www.example.com"><Adbannerside /></Link></div> */}
-				</div>
-				<div className={styles.rightside}>
-					<PromoBlock />
-					<div className={styles.morepromosblock}>
-						<h2 className={styles.morepromosheader}>Еще объявления:</h2>
-						<div className={styles.morepromosrow}>
-							<MorePromosCard />
-							<MorePromosCard />
-							<MorePromosCard />
-						</div>
-					</div>
-				</div>
-
-			</div>
-			<style jsx>{`
-				
-			`}</style>
-		</MainLayout>
-	)
+const Index = () => {
+	return (<>
+		<div>
+			<PromoBlock />
+		</div>
+	</>)
 }
+
+export default Index
