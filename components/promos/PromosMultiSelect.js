dad9fe949495364052e522d6ea24e53fdeba77a8
Lilia <lilyvalitova94@gmail.com>
2022-05-23 17:19:38 +0500

    вывод const

--- components/promos/PromosMultiSelect.js
+++ components/promos/PromosMultiSelect.js
@@ -1,5 +1,6 @@
 import { MultiSelect } from '@mantine/core';
 import { useState, useEffect } from 'react';
+import { categories, darkMet, colorMet } from './const';
 
 export function PromosMultiSelect() {
   const [darkMetallStatus, setDarkMetallStatus] = useState(false)
@@ -7,64 +8,9 @@ export function PromosMultiSelect() {
   const [equipmentStatus, setEquipmentStatus] = useState(false)
   const [rawMaterilStatus, setRawMaterialStatus] = useState(false)
   const [data, setData] = useState([]);
+  
 
-  const categories = [
-    { value: 'Черные металлы', label: 'Черные металлы' },
-    { value: 'Цветные металлы', label: 'Цветные металлы' },
-    { value: 'Оборудование', label: 'Оборудование' },
-    { value: 'Сырье', label: 'Сырье' },
-    { value: 'Транспортные услуги', label: 'Транспортные услуги' },
-    { value: 'Прочие услуги', label: 'Прочие услуги' },
-  ];
-
-  const darkMet = [
-    { value: 'Арматура', label: 'Арматура' },
-    { value: 'Балка', label: 'Балка' },
-    { value: 'Катанка', label: 'Катанка' },
-    { value: 'Квадрат', label: 'Квадрат' },
-    { value: 'Круг', label: 'Круг' },
-    { value: 'Полоса', label: 'Полоса' },
-    { value: 'Рельсы', label: 'Рельсы' },
-    { value: 'Уголок', label: 'Уголок' },
-    { value: 'Шары стальные', label: 'Шары стальные' },
-    { value: 'Швеллер', label: 'Швеллер' },
-    { value: 'Шестигранник', label: 'Шестигранник' },
-    { value: 'Шпунт', label: 'Шпунт' },
-    { value: 'Ларсена', label: 'Ларсена' },
-    { value: 'Жесть', label: 'Жесть' },
-    { value: 'Лист', label: 'Лист' },
-    { value: 'Профнастил', label: 'Профнастил' },
-    { value: 'Труба бесшовная', label: 'Труба бесшовная' },
-    { value: 'Труба вентури', label: 'Труба вентури' },
-    { value: 'Труба водогазопроводная', label: 'Труба водогазопроводная' },
-    { value: 'Болты', label: 'Болты' },
-    { value: 'Винты', label: 'Винты' },
-    { value: 'Гвозди', label: 'Гвозди' },
-    { value: 'Заклепки', label: 'Заклепки' },
-    { value: 'Канаты', label: 'Канаты' },
-    { value: 'Лента', label: 'Лента' },
-    { value: 'Проволока', label: 'Проволока' },
-    { value: 'Саморезы', label: 'Саморезы' },
-    { value: 'Сетка', label: 'Сетка' },
-    { value: 'Шайбы', label: 'Шайбы' },
-    { value: 'Шурупы', label: 'Шурупы' },
-    { value: 'Электроды', label: 'Электроды' },
-    { value: 'Крепеж', label: 'Крепеж' },
-    { value: 'Прочие черные металлы', label: 'Прочие черные металлы' },
-  ];
-
-  const colorMet = [
-    { value: 'Алюминий', label: 'Алюминий' },
-    { value: 'Бронза', label: 'Бронза' },
-    { value: 'Латунь', label: 'Латунь' },
-    { value: 'Медь', label: 'Медь' },
-    { value: 'Никель', label: 'Никель' },
-    { value: 'Олово', label: 'Олово' },
-    { value: 'Свинец', label: 'Свинец' },
-    { value: 'Титан', label: 'Титан' },
-    { value: 'Цинк', label: 'Цинк' },
-    { value: 'Прочие цветные металлы', label: 'Прочие цветные металлы' },
-  ];
+  
   const updateValue = e => {
       if ((e.indexOf('Черные металлы') !== -1) && (darkMetallStatus === false)) {
         setDarkMetallStatus(true)
