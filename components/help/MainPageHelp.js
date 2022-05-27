cdc9c2b94c8775247fe7684f72f67a6e86bed9ce
Lilia <lilyvalitova94@gmail.com>
2022-05-27 15:13:34 +0500

    edit MAinPageHelp

--- components/help/MainPageHelp.js
+++ components/help/MainPageHelp.js
@@ -1,16 +1,35 @@
 import styles from "./MainPageHelp.module.scss"
-import { TextInput, Button, Text} from '@mantine/core';
+import { TextInput, Button, Text, Card,Grid, Textarea} from '@mantine/core';
+
 
 export const MainPageHelp = () =>  {
-    return (<>
-        <div className={styles.maindiv}>
-            <Text color="blue" className={styles.helpheader}>Здесь вы можете задать любой вопрос <strong>о нашем сайте</strong>!</Text>
-            <TextInput className={styles.emailinput}  radius="lg" size="lg" type="text" placeholder="Введите e-mail для обратной связи" />
-            <TextInput className={styles.probleminput}  radius="lg" size="lg" type="text" placeholder="Опишите вашу проблему или вопрос в двух словах" />
-            <TextInput className={styles.problemdesrciptioninput}  radius="lg" size="xl" type="text" placeholder="Расскажите о проблеме немного подробнее" />
-            <Button className={styles.sendbutton} radius="lg">Отправить</Button>
-        </div>
-        <style jsx>{`
-        `}</style>
-    </>)
-}
+     return (
+    <>
+        <Card >
+            <Text align="center" size="xl" color="blue" style={{marginBottom: 20}}>Здесь вы можете задать любой вопрос <strong>о нашем сайте</strong>!</Text>
+                <Grid>
+                    <Grid.Col align="center">
+                        <TextInput style={{width: 800, marginBottom: 15}} placeholder="Введите e-mail для обратной связи" />
+                        <TextInput style={{width: 800, marginBottom: 15}} placeholder="Опишите вашу проблему или вопрос в двух словах" />
+                        <Textarea  minRows={5} style={{width: 800, marginBottom: 20}} placeholder="Расскажите о проблеме немного подробнее" />
+                    </Grid.Col>
+                    <Grid.Col align="center">
+                        <Button style={{marginBottom: 15}} variant="outline">Отправить</Button>
+                    </Grid.Col>
+            </Grid>
+        </Card>
+</>)
+ }
+// export const MainPageHelp = () =>  {
+//     return (<>
+//         <div className={styles.maindiv}>
+//             <Text color="blue" className={styles.helpheader}>Здесь вы можете задать любой вопрос <strong>о нашем сайте</strong>!</Text>
+//             <TextInput className={styles.emailinput}  radius="lg" size="lg" type="text" placeholder="Введите e-mail для обратной связи" />
+//             <TextInput className={styles.probleminput}  radius="lg" size="lg" type="text" placeholder="Опишите вашу проблему или вопрос в двух словах" />
+//             <TextInput className={styles.problemdesrciptioninput}  radius="lg" size="xl" type="text" placeholder="Расскажите о проблеме немного подробнее" />
+//             <Button className={styles.sendbutton} radius="lg">Отправить</Button>
+//         </div>
+//         <style jsx>{`
+//         `}</style>
+//     </>)
+// }
--- pages/account/edit/index.js
+++ pages/account/edit/index.js
@@ -1,31 +1,39 @@
-import Head from 'next/head'
-import { MainLayout } from '../../../components/Layout/MainLayout'
 import { YourData } from '../../../components/editprofile/YourData'
-import { JurData } from '../../../components/editprofile/JurData'
-import { Adbannertop } from '../../../components/Adbannertop'
-import Link from 'next/link'
-import styles from "../../../styles/editprofile.module.scss"
+import { useState } from 'react';
+import { Modal, Button, Group } from '@mantine/core';
 
-const EditProfile = () => {
-    return (
-        <MainLayout>
-            <Head>
-                <title>Next Title</title>
-                <meta name="keywords" content="next, javascript" />
-                <meta name="description" content="this is" />
-                <meta charSet="utf-8" />
-            </Head>
-            {/* <Adbannertop/> */}
-            <div className={styles.content}>
-                <div className={styles.rightside}>
-                    <YourData />
-                    <JurData />
-                </div>
-            </div>
-            <style jsx>{`
-			`}</style>
-        </MainLayout>
-    )
-}
 
-export default EditProfile;
+
+
+
+// import Head from 'next/head'
+// import { MainLayout } from '../../../components/Layout/MainLayout'
+// import { YourData } from '../../../components/editprofile/YourData'
+// import { JurData } from '../../../components/editprofile/JurData'
+// import { Adbannertop } from '../../../components/Adbannertop'
+// import Link from 'next/link'
+// import styles from "../../../styles/editprofile.module.scss"
+
+// const EditProfile = () => {
+//     return (
+//         <MainLayout>
+//             <Head>
+//                 <title>Next Title</title>
+//                 <meta name="keywords" content="next, javascript" />
+//                 <meta name="description" content="this is" />
+//                 <meta charSet="utf-8" />
+//             </Head>
+//             {/* <Adbannertop/> */}
+//             <div className={styles.content}>
+//                 <div className={styles.rightside}>
+//                     <YourData />
+//                     <JurData />
+//                 </div>
+//             </div>
+//             <style jsx>{`
+// 			`}</style>
+//         </MainLayout>
+//     )
+// }
+
+// export default EditProfile;
