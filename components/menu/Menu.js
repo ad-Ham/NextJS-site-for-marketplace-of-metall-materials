0f8f0391c51cab22fb881b8469a834e4fb8cdf62
ad-Ham <aleks.khomenkov.03@mail.ru>
2022-06-03 15:59:30 +0300

    Выход из аккаунта

--- components/menu/Menu.js
+++ components/menu/Menu.js
@@ -3,9 +3,16 @@ import { Menu, Divider, Button} from '@mantine/core';
 import Link from 'next/link'
 import { User, Logout, FileText, MessageCircle, UserCircle} from 'tabler-icons-react';
 import { UserButton } from '../mainlayout/UserButton'
+import { useRouter } from 'next/router'
 
 export function MenuUser () {
+  const router = useRouter();
   const [opened, handlers] = useDisclosure(false);
+
+  const deleteTocken = () => {
+    localStorage.removeItem('token')
+    router.reload(window.location.pathname)
+  }
 
   return (
     <>
@@ -14,7 +21,7 @@ export function MenuUser () {
         <Link href="#" passHref><Menu.Item  icon={<MessageCircle size={14} />}>Мои сообщения</Menu.Item></Link>
         <Link href="/myPromos" passHref><Menu.Item  icon={<FileText size={14} />}>Мои объявления</Menu.Item></Link>
         <Divider />
-        <Link href="#" passHref><Menu.Item color="red" icon={<Logout size={14} />}>Выйти из аккаунта</Menu.Item></Link>
+        <Link href="#" passHref><Menu.Item color="red" icon={<Logout size={14} />} onClick={deleteTocken}>Выйти из аккаунта</Menu.Item></Link>
     </Menu>
 </>  
   );
